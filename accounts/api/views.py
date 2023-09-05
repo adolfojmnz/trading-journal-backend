49594919f5ecf8a4a8ea62b19e1b2a341b8f55dc
Adolfo Jimenez <adolfoj@protonmail.com>
2023-09-05 18:30:21 -0400

    Override `post` method on `TransactionListView` ...
    The changes implemented on this commit, allow the `post` method
    to perform the corresponding updates to the associated trading
    account, for example, increase the value of the `total_deposited`
    and `equity` attributes of the trading account upon a transaction
    of type `DP` (deposi).

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -143,6 +143,34 @@ class TransactionListView(ListCreateAPIView):
     queryset = model.objects.all()
     serializer_class = TransactionSerializer
 
+    def make_deposit(self, trading_account, amount):
+        trading_account.equity += amount
+        trading_account.total_deposited += amount
+        trading_account.save()
+
+    def make_withdrawal(self, trading_account, amount):
+        trading_account.equity -= amount
+        trading_account.total_withdrawn += amount
+        trading_account.save()
+
+    def make_transaction(self, serializer):
+        trading_account = serializer.validated_data["trading_account"]
+        transaction_type = serializer.validated_data["type"]
+        transaction_amount = serializer.validated_data["amount"]
+        if transaction_type == "WT":
+            return self.make_withdrawal(trading_account, transaction_amount)
+        return self.make_deposit(trading_account, transaction_amount)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.make_transaction(serializer)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
 
 class TransactionDetailView(RetrieveUpdateDestroyAPIView):
     model = Transaction
