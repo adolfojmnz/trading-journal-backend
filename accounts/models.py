eab74f06608cd69b22dc824d5556917b01875610
Adolfo Jimenez <adolfoj@protonmail.com>
2023-09-08 17:37:00 -0400

    Users can now set their profile as public or private ...
    New behavior: Users can now set their profile as public or private
    with the attribute `public` which takes a bool value (True or False).
    
    This feature will be used to filter out private profiles while using
    the users endpoints.

--- accounts/models.py
+++ accounts/models.py
@@ -21,6 +21,10 @@ class User(AbstractUser):
 
     for further information refer to https://docs.djangoproject.com/en/4.2/ref/contrib/auth/
     """
+    public = models.BooleanField(
+        default=True,
+        help_text="If True, the profile will be retriverable by other users",
+    )
 
     def __str__(self):
         return self.username
