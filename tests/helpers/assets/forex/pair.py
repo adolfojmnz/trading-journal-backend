a3837749a590dd6c0da6b0c904a5e5c8dacbb759
Adolfo Jimenez <adolfoj@protonmail.com>
2023-09-10 12:45:23 -0400

    Add tests for the `currency-pair-list` endpoint

--- tests/helpers/assets/forex/pair.py
+++ tests/helpers/assets/forex/pair.py
@@ -1,6 +1,6 @@
 from assets.models import CurrencyPair
 
-from tests.data.assets.forex import pairs
+from tests.data.assets.forex import pair
 from tests.helpers.assets.forex import single
 
 
@@ -14,7 +14,7 @@ def create_currency_pair(pair_data, base_currency, quote_currency):
 
 def create_eur_gbp_pair():
     return create_currency_pair(
-        pairs.eur_gbp,
+        pair.eur_gbp,
         base_currency=single.create_eur_currency(),
         quote_currency=single.create_gbp_currency(),
     )
@@ -22,7 +22,7 @@ def create_eur_gbp_pair():
 
 def create_eur_jpy_pair():
     return create_currency_pair(
-        pairs.eur_jpy,
+        pair.eur_jpy,
         base_currency=single.create_eur_currency(),
         quote_currency=single.create_jpy_currency(),
     )
@@ -30,7 +30,7 @@ def create_eur_jpy_pair():
 
 def create_gbp_jpy_pair():
     return create_currency_pair(
-        pairs.gbp_jpy,
+        pair.gbp_jpy,
         base_currency=single.create_gbp_currency(),
         quote_currency=single.create_jpy_currency(),
     )
