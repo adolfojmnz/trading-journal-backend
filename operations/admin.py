44bb4de732466ad9556b7051d1f0aaf0174600ed
Adolfo Jimenez <adolfoj@protonmail.com>
2023-09-10 18:29:37 -0400

    Refactor: rename model `Operation` to `ForexOperation` ...
    This change allows destinguishing forex operations from others.

--- operations/admin.py
+++ operations/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Operation
+from .models import ForexOperation
 
 
-admin.site.register(Operation)
+admin.site.register(ForexOperation)
--- operations/models.py
+++ operations/models.py
@@ -4,37 +4,32 @@ from assets.models import CurrencyPair
 from accounts.models import TradingAccount
 
 
-OPERATION_STATUS = [
+OPERATION_STATUS_CHOICE = [
     ("OP", "open"),
     ("LO", "loss"),
     ("PR", "profit"),
 ]
 
-POSITIONS = [
+OPERATION_POSITION_CHOICE = [
     ("LN", "long"),
     ("SH", "short"),
 ]
 
-class Operation(models.Model):
+
+class ForexOperation(models.Model):
     trading_account = models.ForeignKey(TradingAccount, on_delete=models.PROTECT)
     currency_pair = models.ForeignKey(CurrencyPair, on_delete=models.PROTECT)
-    status = models.CharField(choices=OPERATION_STATUS, max_length=2)
+    status = models.CharField(choices=OPERATION_STATUS_CHOICE, max_length=2)
     open_datetime = models.DateTimeField()
     close_datetime = models.DateTimeField(default=None, blank=True, null=True)
     open_price = models.FloatField()
     close_price = models.FloatField(default=None, blank=True, null=True)
     volume = models.FloatField(default=0.01)
-    position = models.CharField(
-        choices=POSITIONS,
-        max_length=2,
-    )
-    pips = models.FloatField(
-        default=0.0,
-        help_text="Profit/loss in pips",
-    )
+    position = models.CharField(choices=OPERATION_POSITION_CHOICE, max_length=2)
+    pips = models.FloatField(default=0.0, help_text="Profit/loss in pips")
     profit = models.FloatField(
         default=0.0,
-        help_text="In USD Profit/loss of the operation after closed"
+        help_text="In USD Profit/loss of the operation after closed",
     )
 
     def save(self):
