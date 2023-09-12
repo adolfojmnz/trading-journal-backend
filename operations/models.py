021aa59d54125c1663933273a96b2045585b94bf
Adolfo Jimenez <adolfoj@protonmail.com>
2023-09-12 15:50:06 -0400

    Fix logic on ForexOperation save() method ...
    The changes provided on this commit change the logic on the save()
    method of the ForeOperation model. The changes fix the wrong calculation
    of the pip movement of a trade.

--- operations/models.py
+++ operations/models.py
@@ -20,33 +20,35 @@ class ForexOperation(models.Model):
     trading_account = models.ForeignKey(TradingAccount, on_delete=models.PROTECT)
     currency_pair = models.ForeignKey(CurrencyPair, on_delete=models.PROTECT)
     status = models.CharField(choices=OPERATION_STATUS_CHOICE, max_length=2)
+    position = models.CharField(choices=OPERATION_POSITION_CHOICE, max_length=2)
     open_datetime = models.DateTimeField()
     close_datetime = models.DateTimeField(default=None, blank=True, null=True)
     open_price = models.FloatField()
     close_price = models.FloatField(default=None, blank=True, null=True)
     volume = models.FloatField(default=0.01)
-    position = models.CharField(choices=OPERATION_POSITION_CHOICE, max_length=2)
     pips = models.FloatField(default=0.0, help_text="Profit/loss in pips")
     profit = models.FloatField(
         default=0.0,
         help_text="In USD Profit/loss of the operation after closed",
     )
 
-    def save(self):
-        if not self.close_price:
+    def save(self, *args, **kwargs):
+        if not self.close_price or self.pips:
+            # if close_price is 0, pips shouldn't be calculated.
+            # if pips is defined, there's no reasone to calculate it.
             return super().save()
-        if self.position == "LN":
-            self.pips = self.close_price - self.open_price
-        else:
-            self.pips = self.open_price - self.close_price
 
-        self.pips *= self.currency_pair.multiplier
-        self.profit = self.pips * self.volume * self.currency_pair.usd_per_lot
-        return super().save()
+        if self.position == "LN":
+            self.pips = float(self.close_price) - float(self.open_price)
+        else:
+            self.pips = float(self.open_price) - float(self.close_price)
+        self.pips *= (10 ** self.currency_pair.pip_decimal_position)
+
+        return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return f"""
-            {self.position} position on
+            {self.position} on
             {self.currency_pair}:
             {self.pips:.2f} PIPs |
             {self.profit:.2f} USD
