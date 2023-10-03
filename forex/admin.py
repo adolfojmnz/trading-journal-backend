0be5bbfe08a89838b8e4640253f154feb5835c14
Adolfo Jimenez <adolfoj@protonmail.com>
2023-10-03 17:11:40 -0400

    Register `ForexOperation` model

--- forex/admin.py
+++ forex/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from forex.models import Currency, CurrencyPair
+from forex.models import Currency, CurrencyPair, ForexOperation
 
 admin.site.register(Currency)
 admin.site.register(CurrencyPair)
+admin.site.register(ForexOperation)
