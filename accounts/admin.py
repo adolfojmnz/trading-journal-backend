97ee87842b8f30d630157531d687db2508696669
Adolfo Jimenez <adolfoj@protonmail.com>
2023-10-18 16:41:18 -0400

    Customize Admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,4 +3,16 @@ from django.contrib import admin
 from accounts.models import User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_staff",
+        "is_active",
+        "is_superuser",
+        "last_login",
+        "date_joined",
+    ]
--- forex/admin.py
+++ forex/admin.py
@@ -2,6 +2,33 @@ from django.contrib import admin
 
 from forex.models import Currency, CurrencyPair, ForexOperation
 
-admin.site.register(Currency)
-admin.site.register(CurrencyPair)
-admin.site.register(ForexOperation)
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ["symbol", "name", "description"]
+
+
+@admin.register(CurrencyPair)
+class CurrencyPairAdmin(admin.ModelAdmin):
+    list_display = [
+        "symbol",
+        "name",
+        "base_currency",
+        "quote_currency",
+        "pip_decimal_position",
+    ]
+
+
+@admin.register(ForexOperation)
+class ForexOperationAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "ticket",
+        "type",
+        "open_datetime",
+        "close_datetime",
+        "stop_loss",
+        "take_profit",
+        "volume",
+        "pnl"
+    ]
