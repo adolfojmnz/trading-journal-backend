f87ccda077390f711043f9226cf86a65466994fa
Adolfo Jimenez <adolfoj@protonmail.com>
2023-11-17 16:31:47 -0400

    Set proper expiration time for JWT tokens

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
-import os, environ
+import os
+import environ
 from pathlib import Path
 from datetime import timedelta
 
@@ -110,7 +111,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
      'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
      'ROTATE_REFRESH_TOKENS': False,
      'BLACKLIST_AFTER_ROTATION': True
