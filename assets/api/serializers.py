27f5b8ab7fcc84baeba33498c2dfacec850b20ff
Adolfo Jimenez <adolfoj@protonmail.com>
2023-11-28 12:27:35 -0400

    Serialize symbols for both `base` and `quote` currencies

--- assets/api/serializers.py
+++ assets/api/serializers.py
@@ -1,15 +1,33 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from assets.models import Currency, CurrencyPair
 
 
-class CurrencySerializer(serializers.ModelSerializer):
+class CurrencySerializer(ModelSerializer):
     class Meta:
         model = Currency
         fields = "__all__"
 
 
-class CurrencyPairSerializer(serializers.ModelSerializer):
+class CurrencyPairSerializer(ModelSerializer):
+    base_currency_symbol = SerializerMethodField()
+    quote_currency_symbol = SerializerMethodField()
+
+    def get_base_currency_symbol(self, currency_pair):
+        return currency_pair.base_currency.symbol
+
+    def get_quote_currency_symbol(self, currency_pair):
+        return currency_pair.quote_currency.symbol
+
     class Meta:
         model = CurrencyPair
-        fields = "__all__"
+        fields = [
+            "id",
+            "symbol",
+            "name",
+            "base_currency",
+            "base_currency_symbol",
+            "quote_currency",
+            "quote_currency_symbol",
+            "pip_decimal_position",
+        ]
