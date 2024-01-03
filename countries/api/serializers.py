21c2d391dd443d5f1f3cf5df1ca5069a89524f89
Adolfo Jimenez <adolfoj@protonmail.com>
2024-01-03 09:59:52 -0400

    Serialize serveral method fields ...
    The changes on this commit serialize additional fields in
    
      - CountrySerializer: currency_symbol
      - EconomicIndicatorSerializer: country_code, currency_symbol
      - EconomicReportSerializer: economic_indicator_name, economic_indicator_country

--- countries/api/serializers.py
+++ countries/api/serializers.py
@@ -1,4 +1,7 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ModelSerializer,
+    SerializerMethodField,
+)
 
 from countries.models import (
     Country,
@@ -8,18 +11,41 @@ from countries.models import (
 
 
 class CountrySerializer(ModelSerializer):
+    currency_symbol = SerializerMethodField()
+
+    def get_currency_symbol(self, country):
+        return country.currency.symbol
+
     class Meta:
         model = Country
         fields = "__all__"
 
 
 class EconomicIndicatorSerializer(ModelSerializer):
+    country_code = SerializerMethodField()
+    currency_symbol = SerializerMethodField()
+
+    def get_country_code(self, indicator):
+        return indicator.country.code
+
+    def get_currency_symbol(self, indicator):
+        return indicator.country.currency.symbol
+
     class Meta:
         model = EconomicIndicator
         fields = "__all__"
 
 
 class EconomicReportSerializer(ModelSerializer):
+    economic_indicator_name = SerializerMethodField()
+    economic_indicator_country = SerializerMethodField()
+
+    def get_economic_indicator_name(self, report):
+        return report.economic_indicator.name
+
+    def get_economic_indicator_country(self, report):
+        return report.economic_indicator.country.code
+
     class Meta:
         model = EconomicReport
         fields = "__all__"
